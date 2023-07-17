18f17ea8347c31c872a74667ddcad037b1dad26a
damiankadziela81 <damiankadziela02@gmail.com>
2023-07-17 14:22:38 +0200

    Added company section in posting

---
+++ src/main/java/com/example/jobboard/posting/Company.java
@@ -0,0 +1,11 @@
+package com.example.jobboard.posting;
+
+import lombok.Data;
+
+@Data
+class Company {
+    private String name;
+    private String contact;
+    private String website;
+
+}
--- src/main/java/com/example/jobboard/posting/Posting.java
+++ src/main/java/com/example/jobboard/posting/Posting.java
@@ -1,19 +1,17 @@
 package com.example.jobboard.posting;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @AllArgsConstructor
-@Getter
-@Setter
+@Data
 class Posting {
     private Long id;
     private String description;
     private BigDecimal salary;
     private LocalDate expiresAt;
-
+    private Company company;
 }
