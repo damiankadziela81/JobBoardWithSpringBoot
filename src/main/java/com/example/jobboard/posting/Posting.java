ea87a3cf834fdc27341bccdce5412920b5406a07
damiankadziela81 <damiankadziela02@gmail.com>
2023-07-18 12:29:57 +0200

    Added H2 database and JPA integration

--- src/main/java/com/example/jobboard/posting/Posting.java
+++ src/main/java/com/example/jobboard/posting/Posting.java
@@ -1,17 +1,25 @@
 package com.example.jobboard.posting;
 
+import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @AllArgsConstructor
 @Data
-class Posting {
+@Entity
+@NoArgsConstructor
+public class Posting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotNull
@@ -27,6 +35,14 @@ class Posting {
     private LocalDate expiresAt;
 
     @NotNull
-    @Valid
-    private Company company;
+    @Length(min = 1, max = 256)
+    private String name;
+
+    @NotNull
+    @Email
+    private String contact;
+
+    @NotNull
+    @URL
+    private String website;
 }
--- src/main/java/com/example/jobboard/posting/PostingRepository.java
+++ src/main/java/com/example/jobboard/posting/PostingRepository.java
@@ -1,36 +1,8 @@
 package com.example.jobboard.posting;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
-
 @Repository
-class PostingRepository {
-
-    private final Map<Long, Posting> storage = new ConcurrentHashMap<>();
-    private final AtomicLong SEQUENCE = new AtomicLong(0L);
-
-    public List<Posting> findAll() {
-        return List.copyOf(storage.values());
-    }
-
-    public Optional<Posting> findById(Long id) {
-        return Optional.ofNullable(storage.get(id));
-    }
-
-    public Posting save(Posting posting) {
-        if(posting.getId() == null) {
-            posting.setId(SEQUENCE.incrementAndGet());
-        }
-        storage.put(posting.getId(), posting);
-        return posting;
-    }
-
-    public void deleteById(final Long id) {
-        storage.remove(id);
-    }
+public interface PostingRepository extends JpaRepository<Posting, Long> {
 }
